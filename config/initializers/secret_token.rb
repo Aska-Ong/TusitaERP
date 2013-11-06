d6a68455d45a92eb3f605f5b6601d9090139bd9d
Aska Ong <aska.ong91@gmail.com>
2013-11-06 11:24:46 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TusitaERP::Application.config.secret_key_base = 'fd9184c949275a641c92d7a725ef68327c995462b5db69e345a67e0beb39f4d8cada3392993f506d5a1c057315ead38e207cce07b45c4708a3293c0e4d8ced05'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TusitaERP::Application.config.session_store :cookie_store, key: '_TusitaERP_session'
