e73482727a9cf2f3639d45d66cc3b63595a031fe
Aska Ong <aska.ong91@gmail.com>
2013-12-03 10:32:35 +0800

    heroku testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ TusitaERP::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif ckeditor/init.js)
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif Ckeditor.assets)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
