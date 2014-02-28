ad37f2f4b834f1876c3b4fdb96525276d5e540be
Aska Ong <aska.ong91@gmail.com>
2014-03-01 03:09:44 +0800

    heroku testing

---
+++ db/migrate/20140108060136_create_transports.rb
@@ -0,0 +1,11 @@
+class CreateTransports < ActiveRecord::Migration
+  def change
+    create_table :transports do |t|
+      t.string :name
+      t.string :email
+      t.integer :contact_number
+
+      t.timestamps
+    end
+  end
+end
